5189f5f15f56dc6d716e197b163062438fdb7291
sali <sali@uxin2020>
2021-06-10 10:17:09 +0800

    2021年2月1日10:51:10

---
+++ demo/src/main/java/com/example/JVMTest.java
@@ -0,0 +1,38 @@
+package com.example;
+
+/**
+ * Create by Hercules
+ * 2021-06-08 22:39
+ */
+public class JVMTest {
+//-XX:+UseConcMarkSweepGC
+//-Xlog:gc+heap=debug:/hprof/mygc.log
+//-Xms20m
+//-Xmx20m
+//-Xmn6m
+//-XX:+HeapDumpOnOutOfMemoryError
+//-XX:HeapDumpPath=D:/hprof/tcc.hprof
+
+    public static void main(String[] args) throws InterruptedException {
+
+//        try {
+//            List kList = new ArrayList();
+//            while (true) {
+//                kList.add(new JVMTest());
+//            }
+//        } catch (Throwable error) {
+//            error.printStackTrace();
+//        }
+        System.out.println(Runtime.getRuntime().freeMemory() / 1024.0 / 1024);
+        {
+            byte[] b = new byte[2 * 1024 * 1024];
+        }
+
+        int m = 0;
+        System.gc();
+        Thread.sleep(1000);
+        System.out.println("===================");
+        System.out.println(Runtime.getRuntime().freeMemory() / 1024.0 / 1024);
+
+    }
+}
---
+++ demo/src/main/java/com/example/Test1.java
@@ -0,0 +1,22 @@
+package com.example;
+
+/**
+ * Create by Hercules
+ * 2021-06-09 22:01
+ */
+public class Test1 {
+
+    public double add(double k,int m){
+        return k + m;
+    }
+
+    public static void main(String[] args) {
+        {
+            int x = 1;
+            System.out.println(x);
+        }
+
+
+
+    }
+}
